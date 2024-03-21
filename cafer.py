111467bc48643573c56480b27b3cdc54a8e1fe57
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-03-21 23:08:10 +0300

    Update cafer.py

--- cafer.py
+++ cafer.py
@@ -1,2 +1,22 @@
 import os
+import sys
+import struct
+from Crypto.Cipher import AES
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPainter, QFont
+from notifypy import Notify
+import random
+import string
+import userpaths
 import guardshield
+import subprocess
+import time
+import multiprocessing as mp
+import ctypes
+import win32gui
+import win32con
+import win32api
+import math
+import winsound
+import winreg as reg
