2121b74b67b8fd3390a923689b1f56c08a60f7e3
Katilmami <akbalmuhammed668@gmail.com>
2024-03-21 23:51:00 +0300

    Update cafer.py

--- cafer.py
+++ cafer.py
@@ -21,3 +21,39 @@ import math
 import winsound
 import winreg as reg
 import requests
+
+
+class caferisimDialog(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Cafer")
+        self.init_ui()
+    def init_ui(self):
+        layout = QVBoxLayout()
+        self.caferisim_input = QLineEdit()
+        layout.addWidget(self.caferisim_input)
+        ask_caferisim_button = QPushButton("İsmin nedir?")
+        ask_caferisim_button.clicked.connect(self.ask_caferisim)
+        layout.addWidget(ask_caferisim_button)
+        self.setLayout(layout)
+    def ask_caferisim(self):
+        caferisim = self.caferisim_input.text()
+        if caferisim:
+            QMessageBox.information(self, "İsiminiz", f"Merhaba, {caferisim}")
+            self.hmmm_cafer()
+        else:
+            QMessageBox.warning(self, "HATA", "Erişim Reddedildi Lütfen Kendinizi tanıtın.")
+    def hmmm_cafer(self):
+        reply = QMessageBox.question(self, "Cafer", "Tamam mı?", QMessageBox.Yes)
+        if reply == QMessageBox.Yes:
+            QMessageBox.information(self, "TAMAM", "....")
+        else:
+            QMessageBox.information(self, "Hata", "Cafergggg ")
+            
+
+if __caferisim__ == "__main__":
+    app = QApplication(sys.argv)
+    dialog = caferisimDialog()
+    dialog.show()
+    sys.exit(app.exec())
+    
